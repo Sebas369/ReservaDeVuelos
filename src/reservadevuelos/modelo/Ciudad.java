10df088a5577f6220d714e9ada1991d238bf54e8
patosol <pato.sol.1980@gmail.com>
2018-09-30 18:17:30 -0300

    clase asiento con getters y setters

---
+++ src/reservadevuelos/modelo/Ciudad.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package reservadevuelos.modelo;
+
+/**
+ *
+ * @author dario
+ */
+public class Ciudad {
+    private int idCiudad;
+    private String nombreCiudad;
+    private String pais;
+    private boolean vigencia; 
+    
+    
+    //CONSTRUCTOR
+
+    public Ciudad(String nombreCiudad, String pais, boolean vigencia) {
+        
+        this.nombreCiudad = nombreCiudad;
+        this.pais = pais;
+        this.vigencia = vigencia;
+    
+    }
+
+   
+
+   /* public void setIdCiudad(int idCiudad) {
+        this.idCiudad = idCiudad;
+    }*/
+
+    public String getNombreCiudad() {
+        return nombreCiudad;
+    }
+
+    public void setNombreCiudad(String nombreCiudad) {
+        this.nombreCiudad = nombreCiudad;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public boolean getVigencia() {
+        return vigencia;
+    }
+
+    public void setVigencia(boolean vigencia) {
+        this.vigencia = vigencia;
+    }
+    
+   
+    
+        
+    }
+
