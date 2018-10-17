f3ccb7a27f08f8b6e1609f855068bc935f5e3ac9
AleRomero <alexismarianaromero@gmail.com>
2018-10-16 21:02:56 -0300

    Clases vuelo y compra

--- src/reservadevuelos/modelo/Vuelo.java
+++ src/reservadevuelos/modelo/Vuelo.java
@@ -5,10 +5,99 @@
  */
 package reservadevuelos.modelo;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author Usuario
  */
 public class Vuelo {
+    private int idVuelo;
+    private Ciudad ciudadOrigen;
+    private Ciudad ciudadDestino;
+    private String aerolinea;
+    private String aeronave;
+    private LocalDate fechaSalida;
+    private LocalDate fechaLlegada;
+    private String estadoVuelo;
+
+    //Constructor
+    public Vuelo(Ciudad ciudadOrigen, Ciudad ciudadDestino, String aerolinea, String aeronave, LocalDate fechaSalida, LocalDate fechaLlegada, String estadoVuelo) {
+        this.ciudadOrigen = ciudadOrigen;
+        this.ciudadDestino = ciudadDestino;
+        this.aerolinea = aerolinea;
+        this.aeronave = aeronave;
+        this.fechaSalida = fechaSalida;
+        this.fechaLlegada = fechaLlegada;
+        this.estadoVuelo = estadoVuelo;
+    }
+    
+    //Getter y setter
+
+    public int getIdVuelo() {
+        return idVuelo;
+    }
+
+    public void setIdVuelo(int idVuelo) {
+        this.idVuelo = idVuelo;
+    }
+
+    public Ciudad getCiudadOrigen() {
+        return ciudadOrigen;
+    }
+
+    public void setCiudadOrigen(Ciudad ciudadOrigen) {
+        this.ciudadOrigen = ciudadOrigen;
+    }
+
+    public Ciudad getCiudadDestino() {
+        return ciudadDestino;
+    }
+
+    public void setCiudadDestino(Ciudad ciudadDestino) {
+        this.ciudadDestino = ciudadDestino;
+    }
+
+    public String getAerolinea() {
+        return aerolinea;
+    }
+
+    public void setAerolinea(String aerolinea) {
+        this.aerolinea = aerolinea;
+    }
+
+    public String getAeronave() {
+        return aeronave;
+    }
+
+    public void setAeronave(String aeronave) {
+        this.aeronave = aeronave;
+    }
+
+    public LocalDate getFechaSalida() {
+        return fechaSalida;
+    }
+
+    public void setFechaSalida(LocalDate fechaSalida) {
+        this.fechaSalida = fechaSalida;
+    }
+
+    public LocalDate getFechaLlegada() {
+        return fechaLlegada;
+    }
+
+    public void setFechaLlegada(LocalDate fechaLlegada) {
+        this.fechaLlegada = fechaLlegada;
+    }
+
+    public String getEstadoVuelo() {
+        return estadoVuelo;
+    }
+
+    public void setEstadoVuelo(String estadoVuelo) {
+        this.estadoVuelo = estadoVuelo;
+    }
+    
+    
     
 }
